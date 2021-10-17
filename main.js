15e75b30a102017f9e34ee6339b85fa03dc4b0ad
UrbinaBarrios <urbinabarios@gmail.com>
2021-10-17 18:06:32 -0400

    Mi mejor intento de skill con js aunque no tiene json :C perdoname dios

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
+/* mi mejor intento de progress bar */
+
+data = {
+  task1: {
+    progress: 0.2,
+    name: "Python",
+  },
+  task2: {
+    progress: 0.4,
+    name: "Java",
+  },
+  task3: {
+    progress: 0.9,
+    name: "HTML",
+  },
+  task4: {
+    progress: 0.4,
+    name: "Javascript",
+  },
+  task5: {
+    progress: 0.5,
+    name: "CSS",
+  },
+};
+
+wraper_progressbar = document.querySelector(".wraper-progressbar");
+wp = wraper_progressbar;
+
+for (task in data) {
+  elem_label = document.createElement("label");
+
+  elem_span = document.createElement("span");
+
+  elem_span_text = document.createTextNode(data[task].name);
+
+  elem_div_task = document.createElement("div");
+  elem_div_task.setAttribute("id", "task");
+  elem_div_task.classList.add("task");
+
+  elem_div_progress = document.createElement("div");
+  elem_div_progress.classList.add("progress");
+
+  progress = data[task].progress;
+  position = 272 * progress;
+  elem_div_progress.style.left = position + "px";
+  if (progress < 0.25) {
+    elem_div_progress.classList.add("progress-red");
+  } else if (progress < 0.75) {
+    elem_div_progress.classList.add("progress-orange");
+  } else {
+    elem_div_progress.classList.add("progress-green");
+  }
+
+  elem_div_tooltip = document.createElement("div");
+  elem_div_tooltip.classList.add("tooltip");
+  elem_div_tooltip_text = document.createTextNode(progress * 100 + " %");
+
+  elem_span.appendChild(elem_span_text);
+  elem_label.appendChild(elem_span);
+  elem_div_tooltip.appendChild(elem_div_tooltip_text);
+  elem_div_progress.appendChild(elem_div_tooltip);
+  elem_div_task.appendChild(elem_div_progress);
+  elem_label.appendChild(elem_div_task);
+
+  wp.appendChild(elem_label);
+}
+
+/* lo intente depana que si */
+
 const carouselSlide = document.querySelector(".carousel-slide");
 const carouselImgs = document.querySelectorAll(".carousel-slide img");
 
